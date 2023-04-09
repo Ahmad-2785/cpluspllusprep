1687881b440a9dd7928c9a4ea19165ad5cd329c3
Abhishek kumar <abhishekkr8399@gmail.com>
2023-04-09 17:03:03 +0530

    recursion completed

--- 1.Basics/1.5recursion/printname.cpp
+++ 1.Basics/1.5recursion/printname.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int cnt=0;
-void print(string s){
-    if(cnt==3) return; 
-    cout<<s<<endl;
-    cnt++;
-    print(s);
+void print(int i,int n){
+    if(i>n) return; 
+    cout<<"Abhishek"<<endl;
+    print(i+1,n);
 }
 int main(){
-    print("Abhishek");
+    int n;
+    cin >> n;
+    print(1,n);
     return 0;
 }
